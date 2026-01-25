7b7cdd4c35b97cf3a12de77f5597a40d75755182
Fedor Shebanin <fashebanin@edu.hse.ru>
2026-01-25 20:53:51 +0300

    Добавил экзешники для создания и извлечения таблиц

---
+++ src/interface/CreateTable.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+#include "CHSVConverter.h"
+#include "glog/logging.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        DLOG(ERROR) << "Bad files count";
+        return 111;
+    }
+
+    DLOG(INFO) << "Creating table " << argv[3] << '\n';
+
+    std::string schema_file(argv[1]), data_file(argv[2]), table_name(argv[3]);
+
+    cngn::FromCsvToFormat(schema_file, data_file, table_name);
+
+    DLOG(INFO) << "Table created.";
+
+    return 0;
+}

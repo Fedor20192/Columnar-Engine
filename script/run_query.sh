b9e7f8083b6d36a238e265a7e27ac0a38bb91d26
Thinkpad <hui@aboba.ru>
2026-05-25 12:26:10 +0300

    Пусть компилятор работает

--- script/run_query.sh
+++ script/run_query.sh
@@ -6,4 +6,4 @@ COLUMNAR=$2
 OUTPUT=$3
 LOGS=$4
 
-./build-release/src/executable/execute_query "${QUERY_NUM}" "${COLUMNAR}" > "${OUTPUT}" 2> "${LOGS}"
+sudo nice -n -20 ./build-release/src/executable/execute_query "${QUERY_NUM}" "${COLUMNAR}" > "${OUTPUT}" 2> "${LOGS}"

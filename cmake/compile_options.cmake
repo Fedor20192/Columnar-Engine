749ec88f7f637082c6fd643cce189ee640ca6497
Thinkpad <hui@aboba.ru>
2026-05-25 12:36:56 +0300

    -arch=native

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -5,7 +5,7 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -Wpedantic")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -g")
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS} -O3 -ffast-math -flto -DGOOGLE_STRIP_LOG=2")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS} -O3 -ffast-math -flto -march=native -DGOOGLE_STRIP_LOG=2")
 #add_compile_options(-fprofile-arcs -ftest-coverage)
 #add_link_options(-fprofile-arcs -ftest-coverage)
 
